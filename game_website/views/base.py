c240f3db4590956b3c2877d3b72b95efc86665f3
Josh Samara <joshsamara@gmail.com>
2015-02-18 10:45:54 -0500

    Add basic login

--- game_website/views/base.py
+++ game_website/views/base.py
@@ -1,14 +1,17 @@
 from django.views.generic import View
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
-class Home(View):
-    def get(self, *args):
-        html = """
-        <html>
-            <body>
-                <h1>Home</h1>
-                <a href="/admin">Admin page</a>
-            </body>
-        </html>"""
-        return HttpResponse(html)
+class LoginRequiredMixin(object):
+    """Ensures that user must be authenticated in order to access view."""
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+
+
+class Home(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "index.html")
