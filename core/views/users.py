aa64f9732f7a2928d48f2c96bcb08ffac4fa6a1c
farid00 <brandman.m@husky.neu.edu>
2015-02-18 17:21:52 -0500

    User Registration
    Just Changed User Registration link to point to /core/ (all things
    point to core now because if we want to use another app its link will
    be /<second_app>/

--- core/views/users.py
+++ core/views/users.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             new_user = form.save()
             #User needs to be Logged in after creation
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect("/core/")
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {
