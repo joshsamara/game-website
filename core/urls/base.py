843c8a19ce772d219ef5508a0165fcbad6aa0f04
farid00 <brandman.m@husky.neu.edu>
2015-02-18 15:39:25 -0500

    Added core app

---
+++ core/urls/base.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from game_website.views import Home, register
+
+# Use this file to import all other url
+urlpatterns = [
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', Home.as_view(), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/$', include('django.contrib.auth.urls')),
+    url(r'^register/$', register),
+
+]
