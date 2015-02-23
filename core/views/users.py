0e963e27b68fca8f87e17f51e59934b45f45130d
Josh Samara <joshsamara@gmail.com>
2015-02-23 15:42:40 -0500

    Change registration redirect

--- core/views/users.py
+++ core/views/users.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             new_user = form.save()
             #User needs to be Logged in after creation
-            return HttpResponseRedirect("/core/")
+            return HttpResponseRedirect("/")
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {
