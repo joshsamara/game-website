bd2c1b759b6c7fea307bf4e8c00d33c9c336cd44
farid00 <brandman.m@husky.neu.edu>
2015-02-23 12:01:33 -0500

    Adding in Models for Games
    Game Model and Fields added (no approval from advisor yet)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Game(models.Model):
+	game_id = models.IntegerField()
+	game_link = models.CharField(max_length=200)
+	game_image = models.ImageField()
+	game_description = models.charField()
+	game_owner = models.ForeignKey(User)
+	game_date_published = models.DateField()
+	game_author_name = models.CharField(max_length = 75)
+	game_event_name = models.CharField(max_length = 75)
+	game_genre = models.CharField(max_length=50)
