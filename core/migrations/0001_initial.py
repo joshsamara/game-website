16a9b7a0276082b150bf167a747d6165a0b0e85c
Josh Samara <joshsamara@gmail.com>
2015-03-04 16:14:06 -0500

    Regenerate initial migration

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -2,32 +2,89 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import stdimage.models
+import django.utils.timezone
 from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('email', models.EmailField(unique=True, max_length=75)),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=30)),
+                ('is_staff', models.BooleanField(default=False, help_text=b'Designates whether the user can log into this admin site.')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, blank=True)),
+                ('birthday', models.DateField(null=True)),
+                ('gender', models.CharField(blank=True, max_length=1, choices=[(b'M', b'Male'), (b'F', b'Female'), (b'O', b'Other')])),
+                ('public', models.BooleanField(default=False)),
+            ],
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Game',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('game_id', models.IntegerField()),
-                ('game_link', models.CharField(max_length=200)),
-                ('game_image', models.ImageField(upload_to=b'')),
-                ('game_description', models.CharField(max_length=5000)),
-                ('game_date_published', models.DateField()),
-                ('game_author_name', models.CharField(max_length=75)),
-                ('game_event_name', models.CharField(max_length=75)),
-                ('game_genre', models.CharField(max_length=50)),
-                ('game_owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('name', models.CharField(max_length=50)),
+                ('link', models.URLField()),
+                ('image', stdimage.models.StdImageField(upload_to=b'game_images')),
+                ('description', models.TextField(max_length=5000)),
+                ('date_published', models.DateField()),
+                ('event_name', models.CharField(max_length=75)),
+                ('genre', models.CharField(max_length=50)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('members', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='group',
+            field=models.ForeignKey(to='core.Group', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='owner',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='groups',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', verbose_name='groups'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='user_permissions',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions'),
+            preserve_default=True,
         ),
     ]
