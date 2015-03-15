7cd3a4fe83946fa93b89d7094328363351aa2e5a
Josh Samara <joshsamara@gmail.com>
2015-03-15 07:48:38 -0400

    Update template dir for heroku

--- game_website/settings.py
+++ game_website/settings.py
@@ -101,7 +101,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
@@ -109,6 +108,23 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    os.path.join(PROJECT_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'core', 'templates'),
+    root('templates'),
+)
+
 #####
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
@@ -132,12 +148,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "core", "static"),
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-    os.path.join(PROJECT_DIR, 'templates'),
-    os.path.join(BASE_DIR, 'core', 'templates'),
 )
 
 LOGIN_URL = '/login/'
