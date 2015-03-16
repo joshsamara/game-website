a3c95e9d8878adf13bd2b35c4a5758e43e0e3129
farid00 <brandman.m@husky.neu.edu>
2015-03-16 14:54:43 -0400

    Storages can collect static

--- game_website/settings.py
+++ game_website/settings.py
@@ -131,3 +131,17 @@ TEMPLATE_DIRS = (
 LOGIN_URL = 'core:login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+AWS_STORAGE_BUCKET_NAME = 'gamewebsitebucket'
+AWS_ACCESS_KEY_ID = 'AKIAJDDIWRNFJNBBKYMA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_HOST = 's3-us-west-2.amazonaws.com'
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'game_website.custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
