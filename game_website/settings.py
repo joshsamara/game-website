16e0e572c82f1b75b262401fa271a41fb1b76864
Josh Samara <joshsamara@gmail.com>
2015-03-18 15:10:38 -0400

    Don't use AWS locally

--- game_website/settings.py
+++ game_website/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf import global_settings
 import os
-from game_website import local_settings
 from django.core.urlresolvers import reverse
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -133,19 +132,26 @@ LOGIN_URL = 'core:login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-AWS_STORAGE_BUCKET_NAME = 'gamewebsitebucket'
-AWS_ACCESS_KEY_ID = local_settings.AWS_ACCESS_KEY
-AWS_SECRET_ACCESS_KEY = local_settings.AWS_SECRET_KEY
-AWS_S3_HOST = 's3-us-west-2.amazonaws.com'
-# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
-# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
-# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
-# We also use it in the next setting.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+if DJANGO_ENV != 'local':
+    from game_website import local_settings
+    AWS_STORAGE_BUCKET_NAME = 'gamewebsitebucket'
+    AWS_ACCESS_KEY_ID = local_settings.AWS_ACCESS_KEY
+    AWS_SECRET_ACCESS_KEY = local_settings.AWS_SECRET_KEY
+    AWS_S3_HOST = 's3-us-west-2.amazonaws.com'
+    # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+    # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+    # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+    # We also use it in the next setting.
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'game_website.custom_storages.StaticStorage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'rushmanager.custom_storages.MediaStorage'
+    STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'game_website.custom_storages.StaticStorage'
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+    MEDIAFILES_LOCATION = 'media'
+    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+    DEFAULT_FILE_STORAGE = 'rushmanager.custom_storages.MediaStorage'
+else:
+    STATICFILES_LOCATION = 'static'
+    STATIC_URL = "/static/"
+    MEDIAFILES_LOCATION = 'media'
+    MEDIA_URL = "/media/"
