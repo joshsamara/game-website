7877cd5fdfeb0de72bdcc8403c187456105e5f29
farid00 <brandman.m@husky.neu.edu>
2015-03-21 19:32:07 -0400

    git merge of migrations

---
+++ core/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0002_auto_20150318_1928'),
+        ('core', '0006_auto_20150320_1836'),
+    ]
+
+    operations = [
+    ]
