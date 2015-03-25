88b522941136bc0118bba9a730b82c2ad3b39eae
Josh Samara <joshsamara@gmail.com>
2015-03-24 21:51:23 -0400

    PEP257 game_website

--- game_website/custom_storages.py
+++ game_website/custom_storages.py
@@ -1,6 +1,7 @@
-# custom_storages.py
+"""Storage devices."""
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
+
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
