e196ffc9df27c28dc272e8b5bed1e58ee0dcf45d
Josh Samara <joshsamara@gmail.com>
2015-03-30 15:24:41 -0400

    Comment cleanup

--- game_website/config/heroku.py
+++ game_website/config/heroku.py
@@ -9,11 +9,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
