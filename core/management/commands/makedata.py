cbf5e311e5741a75b3273405cebf3bc454d61871
Josh Samara <joshsamara@gmail.com>
2015-04-06 16:50:21 -0400

    Update command to generate tags.

--- core/management/commands/makedata.py
+++ core/management/commands/makedata.py
@@ -1,5 +1,5 @@
 from django.core.management.base import NoArgsCommand
-from core.models import Game, User, Group
+from core.models import Game, User, Group, GameTag
 from uuid import uuid4
 from faker import Faker
 import random
@@ -10,7 +10,8 @@ class Command(NoArgsCommand):
     help = "Create a bunch of dummy data."
 
     @staticmethod
-    def _create_user(fake, prefix):
+    def _generate_user(fake, prefix):
+        """Generate a user object."""
         user_dict = {"email": prefix + '-' + fake.email(),
                      "first_name": fake.first_name(),
                      "last_name": fake.first_name(),
@@ -21,6 +22,7 @@ class Command(NoArgsCommand):
 
     @staticmethod
     def _create_group(fake, users):
+        """Generate and save a group object."""
         name = fake.company()
         g = Group(name=name)
         g.save()
@@ -29,14 +31,24 @@ class Command(NoArgsCommand):
         return g
 
     @staticmethod
-    def _create_game(fake, prefix, group):
+    def _generate_gametag(fake):
+        """Generate a tag object."""
+        return GameTag(value=fake.city())
+
+    @staticmethod
+    def _create_game(fake, prefix, group, tags):
+        """Generate and save a tag object."""
         game_dict = {"name": ' '.join(fake.sentence().split(' ')[:2]).title(),
                      "description": fake.paragraph(),
                      "image": None,
                      "date_published": fake.date_time_this_year(),
                      "group": group,
                      "event_name": fake.word().capitalize()}
-        return Game(**game_dict)
+        g = Game(**game_dict)
+        g.save()
+        g.tags = tags
+        g.save()
+        return g
 
     def handle_noargs(self, **options):
         with warnings.catch_warnings():
@@ -48,7 +60,7 @@ class Command(NoArgsCommand):
             # Make 100 users
             users = []
             for i in range(100):
-                users.append(self._create_user(fake, prefix))
+                users.append(self._generate_user(fake, prefix))
             User.objects.bulk_create(users)
             self.stdout.write('Created 100 Users.')
 
@@ -59,12 +71,18 @@ class Command(NoArgsCommand):
                 self._create_group(fake, users)
             self.stdout.write('Created 20 Groups.')
 
+            # Make 30 tags
+            tags = []
+            for i in range(30):
+                tags.append(self._generate_gametag(fake))
+            GameTag.objects.bulk_create(tags)
+            self.stdout.write('Created 30 GameTags.')
+
             # Make 100 games
-            games = []
+            all_tags = GameTag.objects.all()
             groups = Group.objects.all()
             for i in range(100):
                 group = random.choice(groups)
-                games.append(self._create_game(fake, prefix, group))
-            Game.objects.bulk_create(games)
-
+                tags = random.sample(all_tags, random.randint(0, 5))
+                self._create_game(fake, prefix, group, tags)
             self.stdout.write('Created 100 Games.')
