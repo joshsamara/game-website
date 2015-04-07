49db5b9b7439ac97979373b2d6a05d94f400b364
Mark <m.millstein1234@gmail.com>
2015-04-07 13:23:37 -0400

    Fixed migration issues

---
+++ core/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0011_game_featured'),
+        ('core', '0012_auto_20150406_1611'),
+    ]
+
+    operations = [
+    ]
