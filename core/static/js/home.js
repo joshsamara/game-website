ae2def731718e8ab3d9d67512b7efb99ac16c5c5
Josh Samara <joshsamara@gmail.com>
2015-04-06 21:53:15 -0400

    Cleanup home page js.

--- core/static/js/home.js
+++ core/static/js/home.js
@@ -1,30 +1,34 @@
-var app = angular.module("HomePage", ['ngAnimate'])
+var app = angular.module("HomePage", ['ngAnimate']);
 
 app.controller("ListController", function($scope, $http){
-  // Featured are consistant, set them now
-  $http({url: "/games/api", method: "GET", params: {featured: true}})
-    .success(function(response){
-    $scope.featured = response;
-  })
-
-  function search(term){
-    $http({url: "/games/api", method: "GET", params: {term: term}})
+  // Do a search, set the field in the scope to the response
+  function search(params, field){
+    $http({url: "/games/api", method: "GET", params: params})
       .success(function(response){
-      $scope.games = response;
-    })
+        $scope[field] = response;
+      }
+      );
   }
 
-  search('');
+  // Initial setup
+  search({featured: true}, 'featured');
+  search({term: ''}, 'games');
+  search({top: true}, 'top_games');
+  search({recent: true}, 'recent');
+
+
+  // Make a search function available to the scope
   $scope.search = function(){
-    search($scope.search_term);
-  }
+    search({term: $scope.search_term}, 'games');
+  };
 
-})
+});
 
+// Add a filter to cut down super long strings
 app.filter('summarize', function () {
   return function (input) {
     if (input.length > 100){
-        input = input.substring(0,100) + '...'
+        input = input.substring(0,100) + '...';
     }
       return input;
     };
