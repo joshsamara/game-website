b1120f0f7af97ede5bff71b70fc715744d9fa161
Josh Samara <joshsamara@gmail.com>
2015-04-15 15:34:02 -0400

    Fix bad notificaiton status

--- core/views/notifications.py
+++ core/views/notifications.py
@@ -8,7 +8,7 @@ def base(request, notification_id):
     """URL that marks the notification read then redirects"""
     notification = UserNotification.objects.get(pk=notification_id)
     if notification.user.pk is not request.user.pk:
-        response = HttpResponse(403)
+        response = HttpResponse(status=403)
     else:
         response = HttpResponseRedirect(notification.redirect_url)
         notification.read = True
