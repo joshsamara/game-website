bec13ac1367877a32a3dbc96c1e55193d7bab97e
Mark <m.millstein1234@gmail.com>
2015-04-15 09:35:19 -0400

    Minor tweaks

--- core/models.py
+++ core/models.py
@@ -60,7 +60,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         notifications = UserNotification.objects.filter(user=self).order_by('read')[:5]
         return notifications
 
-
     def get_full_name(self):
         """Return the first_name plus the last_name, with a space in between."""
         full_name = '%s %s' % (self.first_name, self.last_name)
