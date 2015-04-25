6d3bb83fcdfc9ae11df2f7ddeac1d8a8aaed2b5a
Josh Samara <joshsamara@gmail.com>
2015-04-25 17:48:59 -0400

    Move search js to its own file.

--- core/static/js/home.js
+++ core/static/js/home.js
@@ -45,26 +45,3 @@ app.directive('errSrc', function() {
     }
   };
 });
-
-// constructs the suggestion engine
-var games = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  // `states` is an array of state names defined in "The Basics"
-  remote: {
-    url: '/games/api',
-    replace: function(url, query) {
-      return url + "?term=" + query;
-    },
-  }
-});
-
-$('.typeahead').typeahead({
-  hint: true,
-  highlight: true,
-},
-{
-  name: 'games',
-  display: 'name',
-  source: games
-});
